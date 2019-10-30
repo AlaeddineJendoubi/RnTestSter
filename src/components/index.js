1b667c51560f3d5d99ae85d4ad4db2a9be00e003
alaeddine jendoubi <alaeddine.jendoubi@esprit.tn>
2019-10-30 03:59:37 +0100

    Fixing Ui Elements
    a. Background image
    b. Display a speed indicator

--- src/components/index.js
+++ src/components/index.js
@@ -1,40 +1,84 @@
 /* @flow */
 import * as React from "react";
-import { StatusBar, View, Text, Image } from "react-native";
+import { StatusBar, View, Text, Image, ImageBackground, StyleSheet, Dimensions, Alert} from "react-native";
+//Importing Button element from react-native-elements library
+import {Button} from "react-native-elements"
+
+//Setting & importing the background image from assets folder
+import backgroundImage from "../../assets/pylon.jpg";
+//Getting the device width and height
+const deviceWidth = Math.round(Dimensions.get('window').width);
+const deviceHeight = Math.round(Dimensions.get('window').height);
 
 const MainView = () => {
+increaseDroneSpeed = () => {
+
+  };
+
+  decreaseDroneSpeed = () => {
+
+    };
+
+
+
   return (
-    <View>
+    <View >
       <StatusBar barStyle="light-content" />
-      <View
-        style={{
-          width: "100%",
-          height: "100%",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-        <Image
-          source={require("../../assets/sterblue.png")}
-          style={{
-            width: "50%",
-            height: "30%",
-            resizeMode: "contain",
-            position: "absolute",
-            top: "10%"
-          }}
-        />
-        <Text style={{ fontWeight: "bold", fontSize: 26 }}>
-          Sterblue mobile internship test
-        </Text>
-        <Text>
-          Congrats, you have everything up and running, now you can get started
-          with the test!
-        </Text>
-      </View>
+      <ImageBackground source={backgroundImage} style={styles.backgroundImageStyle}>
+        <View style={styles.buttonViewStyle}>
+          <Button
+            buttonStyle={styles.buttonStyle}
+            title="Decrease"
+            onPress= {this.decreaseDroneSpeed}
+          />
+          <Text style={styles.textStyle}>
+            Speed : 10
+          </Text>
+          <Button
+            buttonStyle={styles.buttonStyle}
+            title="Increase"
+            onPress={this.increaseDroneSpeed}
+          />
+          <Button
+            buttonStyle={styles.buttonStyle}
+            title="Detect"
+
+          />
+        </View>
+      </ImageBackground>
+
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  containerStyle: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  backgroundImageStyle: {
+    flex: 1,
+    alignItems : 'center',
+    width: deviceWidth,
+    height: deviceHeight,
+  },
+  textStyle:{
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "white",
+  },
+  buttonStyle: {
+    width:100,
+    marginLeft : '2%',
+    alignItems: 'center'
+  },
+  buttonViewStyle: {
+    marginTop :deviceHeight - deviceHeight/5,
+    width : deviceWidth/2,
+    height : deviceHeight/10,
+    alignItems: 'center',
+    justifyContent :'center',
+    backgroundColor : 'rgba(255, 255, 255, 0.3)',
+    flexDirection: 'row'
+  }
+});
 export default MainView;
